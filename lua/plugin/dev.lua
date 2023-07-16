fcdb6be0d9c85d10c18045f0aaa183cc47c51c29
Miffi <miffi@decadent.rest>
2023-07-17 03:32:48 +0600

    Remove mention of orgmode

--- lua/plugin/dev.lua
+++ lua/plugin/dev.lua
@@ -166,7 +166,6 @@ return {
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
           { name = "luasnip" },
-          { name = "orgmode" },
         }),
       })
     end,
