8cad3f3c6ed9b75436ec014a56d54fd5433803a6
Miffi <miffi@decadent.rest>
2024-05-27 10:35:24 +0600

    conform: Add format toggle commands and `formatexpr`

--- lua/plugin/dev.lua
+++ lua/plugin/dev.lua
@@ -1,5 +1,25 @@
 -- General development plugins
 
+-- Conform nvim commands
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+  if args.bang then
+    -- FormatDisable! will disable formatting just for this buffer
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = "Disable autoformat-on-save",
+  bang = true,
+})
+
+vim.api.nvim_create_user_command("FormatEnable", function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = "Re-enable autoformat-on-save",
+})
+
 return {
   {
     "windwp/nvim-autopairs",
@@ -17,7 +37,11 @@ return {
 
   { -- Autoformat
     "stevearc/conform.nvim",
-    lazy = false,
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    init = function()
+      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
     keys = {
       {
         "<leader>f",
@@ -28,16 +52,16 @@ return {
           }
         end,
         mode = "n",
-        desc = "[F]ormat buffer",
+        desc = "Format buffer",
       },
     },
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        local disable_filetypes = {}
         return {
           timeout_ms = 500,
           lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
