ea95148db3f71a53f66f0471332331b47ed82563
Miffi <miffi@decadent.rest>
2024-06-25 16:48:34 +0600

    Fix styling

--- lua/plugin/languages.lua
+++ lua/plugin/languages.lua
@@ -1,7 +1,9 @@
 vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = { "sql", "mysql", "plsql" },
   callback = function()
-    require("cmp").setup.buffer { sources = { { name = "vim-dadbod-completion" } } }
+    require("cmp").setup.buffer {
+      sources = { { name = "vim-dadbod-completion" } },
+    }
   end,
 })
 
