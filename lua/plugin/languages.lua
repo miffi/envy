b0b4d2655128ea404164d8f0f12612555ce25c05
Miffi <miffi@decadent.rest>
2024-06-25 11:52:24 +0600

    Add vim-dadbod

--- lua/plugin/languages.lua
+++ lua/plugin/languages.lua
@@ -1,3 +1,10 @@
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "sql", "mysql", "plsql" },
+  callback = function()
+    require("cmp").setup.buffer { sources = { { name = "vim-dadbod-completion" } } }
+  end,
+})
+
 return {
   {
     "lervag/vimtex",
@@ -22,6 +29,28 @@ return {
     end,
     init = function()
       vim.g.mkdp_combine_preview = 1
+    end,
+  },
+
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+      { "tpope/vim-dadbod", lazy = true },
+      {
+        "kristijanhusak/vim-dadbod-completion",
+        ft = { "sql", "mysql", "plsql" },
+        lazy = true,
+      },
+    },
+    cmd = {
+      "DBUI",
+      "DBUIToggle",
+      "DBUIAddConnection",
+      "DBUIFindBuffer",
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
     end,
   },
 
