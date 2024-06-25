88be90649e1ba21d28e143c262e7bff707c5c908
Miffi <miffi@decadent.rest>
2024-06-25 11:51:27 +0600

    Remove signature keybind in favor of default

--- lua/plugin/lsp.lua
+++ lua/plugin/lsp.lua
@@ -12,7 +12,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
     vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
     vim.keymap.set("n", "<space>da", vim.lsp.buf.add_workspace_folder, opts)
     vim.keymap.set("n", "<space>dr", vim.lsp.buf.remove_workspace_folder, opts)
     vim.keymap.set("n", "<space>dl", function()
